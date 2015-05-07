b205f62fee6b8bf193aff16bf7ff3c426b361a75
shyam <xshyamx@gmail.com>
2015-05-07 12:04:22 +0530

    removing intellij plugin and converting to a maven project

--- src/javarepl/console/ConsoleStatus.java
+++
@@ -1,9 +0,0 @@
-package javarepl.console;
-
-public enum ConsoleStatus {
-    Idle, Starting, Running, Terminating, Terminated;
-
-    public Boolean isRunning() {
-        return this == Running;
-    }
-}
